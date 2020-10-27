fc7025d41ca013a9a1de62c2bb65f6724b7bf4c4
quinnfinite <quinn.royston@gmail.com>
2020-10-26 18:56:33 -0700

    built functioning resolvers and started playing with reduce

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,14 +3,34 @@
 module.exports = {
   Query: {
     animals: (_,__,{})=> {
-      return [
-        {
-          "name": "Sebastian",
-          "animal_type": "Camel",
-          "noise": "I am thirsty"
-        }
-      ]
+      return animalData
+    },
+    animal: (_, queryArgs, {})=> {
+      return animalData.reduce((acc, currentAnimal)=> {
+          let add = true;
+          for (var key in queryArgs) {
+            if(queryArgs[key] !== currentAnimal[key]) add = false;
+          }
+          if (add) acc.push(currentAnimal)
+          return acc
+        },[])
 
+        // console.log(data)
+
+        // return [animalData[0]]
     }
   }
 }
+
+var animalData = [
+  {
+    "name": "Sebastian",
+    "type": "Camel",
+    "noise": "I am thirsty"
+  },
+  {
+    "name": "Bart",
+    "type": "Bat",
+    "noise": "moo"
+  }
+]
--- server/schema.js
+++ server/schema.js
@@ -3,10 +3,11 @@ const { gql } = require('apollo-server');
 const typeDefs= gql`
  type Query {
    animals: [Animal]!
+   animal(type: String, name: String): [Animal]!
  }
  type Animal {
    name: String!
-   animal_type: String!
+   type: String!
    noise: String!
  }
 `
